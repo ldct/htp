2951b400080ae244a26be9a32b84686a48af6c4f
Li Xuanji <xuanji@gmail.com>
2015-08-15 15:20:53 +0800

    read program

--- main.hs
+++ main.hs
@@ -9,12 +9,6 @@ main :: IO ()
 main = do
   args <- getArgs
   case (args) of
-    ["c"] -> putStr . compile $ prog
-    ["i"] -> runProgram prog
-    _     -> putStrLn "Usage: `./main c` or `./main i`"
-
-prog :: Program
-{-- prog = [Assign 'a' (Add (Val 2) (Val 3)),
-        Read 'b',
-        Print (Add (Var 'a') (Var 'b'))] --}
-prog = parseProgram . unlines $ ["assign a 3 * 2 + 3 + 4", "read b", "assign b b + 1", "print a + b"]
+    ["c" ,filename] -> (putStr . compile . parseProgram) =<< readFile filename
+    ["i" ,filename] -> (runProgram . parseProgram) =<< readFile filename
+    _     -> putStrLn "Usage: `./main c filename` or `./main i filename`"
