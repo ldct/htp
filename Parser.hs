30c7f12f6149f8a8c49ddede694a67f86ce5048b
Matthew Chan <matt@themattchan.com>
2015-08-15 14:07:18 -0700

    command parser

--- Parser.hs
+++ Parser.hs
@@ -1,7 +1,7 @@
 -- TODO: replace all uses of head and tail with pattern matching/total functions
 module Parser where
 import Text.Parsec hiding (runParser)
-import Data.Char (isDigit, isAlpha)
+import Data.Char          (isDigit, isAlpha)
 import Control.Monad.Identity
 
 import Ast
@@ -9,34 +9,32 @@ import Ast
 type Parser a = ParsecT [Char] () Identity a
 
 parseProgram :: String -> Program
-parseProgram = map (either (error . show) id . runParser commandParser) . lines
+parseProgram = map (resolveError . parseCommand) . lines
 
 runParser :: Parser a -> String -> Either ParseError a
 runParser parser = parse parser "(source)"
 
+parseCommand = runParser commandParser
+parseExpr = runParser exprParser
+
+resolveError = either (error . show) id
+
 commandParser :: Parser Command
 commandParser = do
   command <- many1 letter
-
   spaces
-
-  rest <- many1 anyChar
-  let args = words rest
+  expr <- many1 anyChar
+  let split = words expr
 
   return $ case command of
-    "print" -> Print $
-      case (runParser exprParser rest) of
-        Right expr -> expr
-        Left  err  -> (error . show) err
+    "print"  ->  Print . resolveError $ parseExpr expr
+    "assign" ->
+      let ([ident] : value1) = split
+          value = resolveError . parseExpr . unwords $ value1
+      in Assign ident value
 
-    "assign" -> Assign (head . head $ args) $
-      case (runParser exprParser (unwords . tail $ args)) of
-        Right expr -> expr
-        Left  err  -> (error . show) err
-
-    "read" -> Read (head . head $ args)
-
-    _ -> error "Invalid command"
+    "read"   -> let [ident] = expr in Read ident
+    _        -> error "Invalid command"
 
 exprParser :: Parser Expr
 exprParser = do
@@ -48,21 +46,21 @@ exprParser = do
   spaces
 
   rest <- many anyChar
-  let item = if (all isDigit itemRaw)
-             then Val (read itemRaw)
+  let item = if all isDigit itemRaw
+             then Val $ read itemRaw
              else if (isAlpha . head) itemRaw
-             then Var (head itemRaw)
+             then Var $ head itemRaw
              else error "invalid expression"
 
   return $ case rest of
-    ""       -> item
+    []       -> item
     ('+':xs) -> Add item (parseRest xs)
     ('-':xs) -> Sub item (parseRest xs)
     ('*':xs) -> Mul item (parseRest xs)
     ('/':xs) -> Div item (parseRest xs)
     other    -> error . show $ other
   where
-  parseRest :: String -> Expr
-  parseRest rest = case (runParser exprParser rest) of
-    Right expr -> expr
-    Left  err  -> (error . show) err
+    parseRest :: String -> Expr
+    parseRest rest = case runParser exprParser rest of
+      Right expr -> expr
+      Left  err  -> (error . show) err
