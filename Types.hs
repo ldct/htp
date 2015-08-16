6f6a30c2dc767df731ad0bf6aede73226a39a478
Matthew Chan <matt@themattchan.com>
2015-08-15 19:33:11 -0700

    Revert "extend types for ITE"
    This reverts commit a4b5b29372255caba1d454a3a0f745b35d005df8.

--- Types.hs
+++ Types.hs
@@ -2,9 +2,8 @@ module Types where
 import qualified Data.Map.Strict as M
 import Data.List (intersperse)
 
---type Value    = Int | Bool
-data Value = IntVal Int | BoolVal Bool
-type Variable = Char -- ['a'..'z'] ++ ['A'..'Z']
+type Value    = Int
+type Variable = Char
 
 type Env = M.Map Variable Value
 
@@ -14,36 +13,24 @@ type Program = [Command]
 
 data Command
   = Print  Expr
-  | Read   Variable
-  | Assign Variable Expr
+  | Read   Char
+  | Assign Char Expr
 
 instance Show Command where
   show (Read name)        = "read " ++ [name]
   show (Assign name expr) = "assign " ++ [name] ++ show expr
   show (Print expr)       = "print " ++ show expr
 
--- should distinguish between the types of expressions...
 data Expr
-  = Op Binop Expr Expr
+  = Op Arith Expr Expr
   | Val Value
   | Var Variable
-  | ITE Expr Expr Expr
-
-data Binop = Arith | Cmpr
 
 data Arith
   = Add
   | Sub
   | Mul
   | Div
-
-data Cmpr
-  = Gt                          -- >  :: Int -> Int -> Bool
-  | Gte                         -- >= :: Int -> Int -> Bool
-  | Lt                          -- <  :: Int -> Int -> Bool
-  | Lte                         -- <= :: Int -> Int -> Bool
-  | Eq                          -- == :: Value -> Value -> Bool
-  | Neq                         -- != :: Value -> Value -> Bool
 
 instance Show Expr where
   show (Val val)  = show val
