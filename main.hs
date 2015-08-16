92ec51ef50117d504603ee10a894af4d2f817302
Neil Parikh <parikh.neil@me.com>
2015-08-16 03:57:12 -0700

    Rough implementaion of the server
    mostly works

--- main.hs
+++ main.hs
@@ -1,36 +1,49 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import System.Environment (getArgs)
-import Interpreter        (runProgram)
-import Compiler           (compile)
-import Debugger           (debug)
-import Parser             (parseProgram)
+import Interpreter        (initialEnv)
+import Debugger           (ProgramState, stepForward, stepBackward, replaceLine)
+import Parser             (parseProgram, runParser, commandParser, resolveError)
 
-import Data.Text
+import Data.Text (Text, pack, unpack)
 import qualified Network.WebSockets as WS
-import Control.Concurrent (MVar, newMVar, modifyMVar_, modifyMVar, readMVar)
+import Control.Concurrent (MVar, newEmptyMVar,  putMVar, tryTakeMVar, readMVar)
+import System.IO (hFlush, stdout)
 
 import           Control.Monad      (forever)
-import qualified Data.Text          as T
 import qualified Network.WebSockets as WS
 
-type Client = (Text, WS.Connection)
-type ServerState = [Client]
+type States = [ProgramState]
 
-meow :: WS.Connection -> IO ()
-meow conn = forever $ do
-    msg <- WS.receiveData conn
-    WS.sendTextData conn $ msg `T.append` ", meow"
-
-newServerState :: ServerState
-newServerState = []
-
-application :: MVar ServerState -> WS.ServerApp
+application :: MVar States -> WS.ServerApp
 application state pending = do
-    conn <- WS.acceptRequest pending
-    WS.forkPingThread conn 30
+  conn <- WS.acceptRequest pending
+  WS.forkPingThread conn 30
+  forever $ do
+    msg <- (WS.receiveData conn :: IO Text)
+    maybeProgStates <- tryTakeMVar state
+    case maybeProgStates of
+      Nothing -> do
+        putMVar state [(parseProgram . unpack $ msg, [], initialEnv, [], [])]
+      Just progStates -> do
+        let action = words . unpack $ msg
+        case action of
+          ["f"] -> putMVar state (stepForward progStates)
+          ["b"] -> putMVar state (stepBackward progStates)
+          ["r", num, line] -> do -- r closes the connection; fix?
+            let command = (resolveError . runParser commandParser) line
+            let offset = read num
+            putMVar state (map (uncurry $ replaceLine command) (zip (map (+ offset) [0..]) progStates))
+          _ -> putMVar state progStates
+        newStates <- readMVar state
+        let (restProgram, executed, env, stdout, stdin):_ = newStates
+        (WS.sendTextData conn) . pack $ "stdin\n" ++ (unlines stdin)
+        (WS.sendTextData conn) . pack $ "stdout\n" ++ (unlines . reverse $ stdout)
+        (WS.sendTextData conn) . pack $ "linesRemaining\n" ++ (show . length $ restProgram)
+        (WS.sendTextData conn) . pack $ "prog\n" ++ (unlines . (map show) $ (reverse executed) ++ restProgram)
+    return ()
 
 main :: IO ()
 main = do
-    state <- newMVar newServerState
-    WS.runServer "0.0.0.0" 9160 $ application state
+  state <- newEmptyMVar
+  WS.runServer "0.0.0.0" 9160 $ application state
